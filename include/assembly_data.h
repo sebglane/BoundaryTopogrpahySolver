c8248d603c3cd6f2eb2d2329b0f79b64e1e5a244
sebglane <glane@tu-berlin.de>
2019-01-02 13:07:15 -0800

    added assembly_data

---
+++ include/assembly_data.h
@@ -0,0 +1,79 @@
+/*
+ * assembly_data.h
+ *
+ *  Created on: Dec 24, 2018
+ *      Author: sg
+ */
+
+#ifndef INCLUDE_ASSEMBLY_DATA_H_
+#define INCLUDE_ASSEMBLY_DATA_H_
+
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/tensor.h>
+
+#include <deal.II/fe/fe_base.h>
+#include <deal.II/fe/mapping.h>
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <vector>
+
+namespace Assembly {
+
+using namespace dealii;
+
+template<int dim>
+struct Scratch
+{
+    Scratch(const FiniteElement<dim>   &finite_element,
+            const Quadrature<dim>      &quadrature,
+            const Quadrature<dim-1>    &face_squadrature,
+            const UpdateFlags           update_flags,
+            const UpdateFlags           face_update_flags);
+
+    Scratch(const Scratch<dim>    &scratch);
+
+    FEValues<dim>               fe_values;
+    FEFaceValues<dim>           fe_face_values;
+
+    // density part
+    std::vector<double>         phi_density;
+    std::vector<Tensor<1,dim>>  grad_phi_density;
+
+    std::vector<double>         present_density_values;
+    std::vector<Tensor<1,dim>>  present_density_gradients;
+    std::vector<double>         present_face_density_values;
+
+    // momentum part
+    std::vector<double>         div_phi_velocity;
+    std::vector<Tensor<1,dim>>  phi_velocity;
+    std::vector<Tensor<2,dim>>  grad_phi_velocity;
+    std::vector<double>         phi_pressure;
+
+    std::vector<double>         present_velocity_divergences;
+    std::vector<Tensor<1,dim>>  present_velocity_values;
+    std::vector<Tensor<2,dim>>  present_velocity_gradients;
+    std::vector<double>         present_pressure_values;
+
+    std::vector<Tensor<1,dim>>  present_face_velocity_values;
+};
+
+template <int dim>
+struct CopyData
+{
+    CopyData(const FiniteElement<dim>   &finite_element);
+    CopyData(const CopyData<dim>        &data);
+
+    FullMatrix<double>                      local_matrix;
+    Vector<double>                          local_rhs;
+
+    std::vector<types::global_dof_index>    local_dof_indices;
+};
+
+} // namespace Assembly
+
+
+#endif /* INCLUDE_ASSEMBLY_DATA_H_ */
---
+++ source/assembly_data.cc
@@ -0,0 +1,99 @@
+/*
+ * assembly_data.cc
+ *
+ *  Created on: Dec 24, 2018
+ *      Author: sg
+ */
+
+#include "assembly_data.h"
+
+namespace Assembly {
+
+template<int dim>
+Scratch<dim>::Scratch(const FiniteElement<dim>  &finite_element,
+                      const Quadrature<dim>     &quadrature,
+                      const Quadrature<dim-1>   &face_quadrature,
+                      const UpdateFlags          update_flags,
+                      const UpdateFlags          face_update_flags)
+:
+fe_values(finite_element,
+          quadrature,
+          update_flags),
+fe_face_values(finite_element,
+               face_quadrature,
+               face_update_flags),
+// density part
+phi_density(finite_element.dofs_per_cell),
+grad_phi_density(finite_element.dofs_per_cell),
+present_density_values(quadrature.size()),
+present_density_gradients(quadrature.size()),
+present_face_density_values(face_quadrature.size()),
+// momentum part
+div_phi_velocity(finite_element.dofs_per_cell),
+phi_velocity(finite_element.dofs_per_cell),
+grad_phi_velocity(finite_element.dofs_per_cell),
+phi_pressure(finite_element.dofs_per_cell),
+present_velocity_divergences(quadrature.size()),
+present_velocity_values(quadrature.size()),
+present_velocity_gradients(quadrature.size()),
+present_pressure_values(quadrature.size()),
+present_face_velocity_values(face_quadrature.size())
+{}
+
+
+template<int dim>
+Scratch<dim>::Scratch(const Scratch<dim>  &scratch)
+:
+fe_values(scratch.fe_values.get_mapping(),
+          scratch.fe_values.get_fe(),
+          scratch.fe_values.get_quadrature(),
+          scratch.fe_values.get_update_flags()),
+fe_face_values(scratch.fe_face_values.get_mapping(),
+               scratch.fe_face_values.get_fe(),
+               scratch.fe_face_values.get_quadrature(),
+               scratch.fe_face_values.get_update_flags()),
+// density part
+phi_density(scratch.phi_density),
+grad_phi_density(scratch.grad_phi_density),
+present_density_values(scratch.present_density_values),
+present_density_gradients(scratch.present_density_gradients),
+present_face_density_values(scratch.present_face_density_values),
+// momentum part
+div_phi_velocity(scratch.div_phi_velocity),
+phi_velocity(scratch.phi_velocity),
+grad_phi_velocity(scratch.grad_phi_velocity),
+phi_pressure(scratch.phi_pressure),
+present_velocity_divergences(scratch.present_velocity_divergences),
+present_velocity_values(scratch.present_velocity_values),
+present_velocity_gradients(scratch.present_velocity_gradients),
+present_pressure_values(scratch.present_pressure_values),
+present_face_velocity_values(scratch.present_face_velocity_values)
+{}
+
+template<int dim>
+CopyData<dim>::CopyData(const FiniteElement<dim>    &finite_element)
+:
+local_matrix(finite_element.dofs_per_cell,
+             finite_element.dofs_per_cell),
+local_rhs(finite_element.dofs_per_cell),
+local_dof_indices(finite_element.dofs_per_cell)
+{}
+
+
+template<int dim>
+CopyData<dim>::CopyData(const CopyData<dim>   &data)
+:
+local_matrix(data.local_matrix),
+local_rhs(data.local_rhs),
+local_dof_indices(data.local_dof_indices)
+{}
+
+}  // namespace Assembly
+
+
+// explicit instantiation
+template struct Assembly::Scratch<2>;
+template struct Assembly::Scratch<3>;
+
+template struct Assembly::CopyData<2>;
+template struct Assembly::CopyData<3>;
